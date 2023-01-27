387f6cb6e36d4302702294fb17b3d9e6c27004cb
Julian Ostmo <julian.ostmo@ibm.com>
2023-01-27 15:16:11 +0000

    feat: bdd testing

---
+++ features/Dummy_steps.js
@@ -0,0 +1,9 @@
+const { Given, When, Then, Fusion } = require('jest-cucumber-fusion');
+
+Given('write given statement here', () => {});
+
+When('write when statement here', () => {});
+
+Then('write then statement here', () => {});
+
+Fusion('Dummy.feature');

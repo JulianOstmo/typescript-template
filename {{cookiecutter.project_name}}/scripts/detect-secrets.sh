07c536dc5da02dc204e406f9807f949d8ec64342
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2023-01-31 17:39:57 +0000

    chore: fix up detect secrets to work with the new images

--- {{cookiecutter.project_name}}/scripts/detect-secrets.sh
+++ {{cookiecutter.project_name}}/scripts/detect-secrets.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -i
 
 shopt -s expand_aliases
+if [[ -z ${INIT_CWD} ]]; then
+    INIT_CWD=$(pwd)
+fi
 
 if [[ $1 == "audit" ]]; then
-    docker run -it --rm -v ${INIT_CWD}:/code ibmcom/detect-secrets:latest audit .secrets.baseline
+    docker run -it --rm -v ${INIT_CWD}:/code icr.io/git-defenders/detect-secrets:redhat-ubi audit --report --fail-on-unaudited --fail-on-live --fail-on-audited-real .secrets.baseline
 elif [[ $1 == "exclude" ]]; then
-    docker run --rm -v ${INIT_CWD}:/code ibmcom/detect-secrets:latest scan --update .secrets.baseline --exclude-files
+    docker run --rm -v ${INIT_CWD}:/code icr.io/git-defenders/detect-secrets:redhat-ubi scan --update .secrets.baseline --exclude-files $2
 elif [[ $1 == "update-baseline" ]]; then
-    docker run --rm -v ${INIT_CWD}:/code ibmcom/detect-secrets:latest scan --update .secrets.baseline
+    docker run --rm -v ${INIT_CWD}:/code  icr.io/git-defenders/detect-secrets:redhat-ubi scan --update .secrets.baseline
 else
-    docker run --rm -v ${INIT_CWD}:/code ibmcom/detect-secrets-hook:latest --baseline .secrets.baseline
+    docker run --rm -v ${INIT_CWD}:/code icr.io/git-defenders/detect-secrets-hook:latest --baseline .secrets.baseline $@
 fi
